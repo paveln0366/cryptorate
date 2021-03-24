1ab577c31059078ea2aafbe64478837ca65c3824
Pavel Potapov <paveln0366@gmail.com>
2021-03-25 01:16:04 +0300

    Добавил адаптер

--- app/src/main/java/com/pavelpotapov/cryptorate/CoinPriceListActivity.kt
+++ app/src/main/java/com/pavelpotapov/cryptorate/CoinPriceListActivity.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.pavelpotapov.cryptorate.adapters.CoinInfoAdapter
+import com.pavelpotapov.cryptorate.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
 
@@ -13,14 +16,17 @@ class CoinPriceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
-        // Новый вариант androidx.lifecycle.ViewModelProviders
-        viewModel = ViewModelProviders.of(this).get(CoinViewModel::class.java)
-//        viewModel.priceList.observe(this, Observer {
-//            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
-//        })
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
 
-        viewModel.getDetailInfo("BTC").observe(this, Observer {
-            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        viewModel = ViewModelProviders.of(this).get(CoinViewModel::class.java)
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
         })
     }
 }
