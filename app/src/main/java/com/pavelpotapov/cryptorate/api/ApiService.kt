6c5ca4b02496801ebbe2f403fe68d92f8ca13736
Pavel Potapov <paveln0366@gmail.com>
2021-04-19 02:49:54 +0300

    Добавил API key

--- app/src/main/java/com/pavelpotapov/cryptorate/api/ApiService.kt
+++ app/src/main/java/com/pavelpotapov/cryptorate/api/ApiService.kt
@@ -6,21 +6,21 @@ import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-// Здесь хранятся все методы по работе с сетью
+// Все методы по работе с сетью
 interface ApiService {
     // Получить список самых популярных валют
     @GET("top/totalvolfull")
     fun getTopCoinsInfo(
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
         @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
         @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
     ): Single<CoinInfoListOfData>
 
 
-    // Загружает полную информацию о валюте
+    // Получить полную информацию о валюте
     @GET("pricemultifull")
     fun getFullPriceList(
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = API_KEY,
         @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
         @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
     ): Single<CoinPriceInfoRawData>
@@ -33,6 +33,7 @@ interface ApiService {
         private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
 
         private const val CURRENCY = "USD"
+        private const val API_KEY = "830a7fafc99d8022f8495a1accd10bcc431e0606f5c3e8f39b9695e0fe9fbae6"
     }
 
 }
